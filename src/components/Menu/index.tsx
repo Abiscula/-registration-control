632785f15c4b9c7cca8288aafa6461c86c25e968
Victor <victorabiscula@live.com>
2022-06-15 11:47:43 -0300

    criado componente de menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,17 @@
+import { ChartLineUp, House, Notepad, NotePencil, PencilSimple } from "phosphor-react";
+import { Link } from "react-router-dom";
+import { Container } from "./style";
+
+export function Menu() {
+    return (
+        <Container>
+            <nav>
+                <Link to="#" className="link"><House size={32} />Home</Link>
+                <Link to="#" className="link"><PencilSimple size={32} />Cadastrar Pessoa</Link>
+                <Link to="#" className="link"><NotePencil size={32} />Cadastrar Contrato</Link>
+                <Link to="#" className="link"><Notepad size={32} />Contratos</Link>
+                <Link to="#" className="link"><ChartLineUp size={32} />Estatísticas</Link>
+            </nav>
+        </Container>
+    )
+}
