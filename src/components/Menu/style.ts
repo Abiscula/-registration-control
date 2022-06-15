5b1604a96921706163b224f2df09534171f80f7d
Victor <victorabiscula@live.com>
2022-06-15 11:47:53 -0300

    estilização do menu

---
+++ src/components/Menu/style.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import '../../variables.css'
+
+export const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    background-color: var(--background-menu);
+    height: 100vh;
+    width: 150px;
+
+    nav {
+        display: flex;
+        flex-direction: column;
+        margin: auto 0;
+        align-items: center;
+        gap: 4.2rem;
+
+        .link {
+            color: var(--color-white);
+            text-decoration: none;
+            width: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin: auto;
+            gap: 2px;
+            text-align: center;
+  
+        }
+
+        .link:hover {
+            color: var(--color-wheat);
+            transform: scale(1.1);
+        }
+
+    }
+`
