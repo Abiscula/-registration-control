10b57a5b74e45f62105a1b33e1457e74a0cfcaff
Victor <victorabiscula@live.com>
2022-06-15 16:40:13 -0300

    aprimorado estruturado do Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,8 +6,8 @@ export function Menu() {
     return (
         <Container>
             <nav>
-                <Link to="#" className="link"><House size={32} />Home</Link>
-                <Link to="#" className="link"><PencilSimple size={32} />Cadastrar Pessoa</Link>
+                <Link to="/" className="link"><House size={32} />Home</Link>
+                <Link to="/register-person" className="link"><PencilSimple size={32} />Cadastrar Pessoa</Link>
                 <Link to="#" className="link"><NotePencil size={32} />Cadastrar Contrato</Link>
                 <Link to="#" className="link"><Notepad size={32} />Contratos</Link>
                 <Link to="#" className="link"><ChartLineUp size={32} />Estatísticas</Link>
