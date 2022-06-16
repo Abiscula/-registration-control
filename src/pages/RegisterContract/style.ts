e0d4da064a407e0903cfcac776d26f7cebe2d4e8
Victor <victorabiscula@live.com>
2022-06-16 18:34:02 -0300

    criado estilização para pagina de contratos

---
+++ src/pages/RegisterContract/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+     display: flex;
+    flex-direction: column;
+    width: 800px;
+    min-width: 600px;
+    height: 75vh;
+    background-color: var(--color-white);
+    border-radius: 60px;
+    margin: auto;
+    padding: 5rem;
+`
