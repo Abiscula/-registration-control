29b57bd12042ac61065bbc832fce2c0bf861091a
Victor <victorabiscula@live.com>
2022-06-16 11:28:46 -0300

    separado tipagem da page de cadastro para ser reaproveitada em outros locais

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface userProps {
+    nome: string
+    sobrenome: string
+    nasc: string
+    cpf: string
+    email: string
+    tel: string
+    bairro: string
+    cep: string
+    localidade: string
+    logradouro: string
+    uf: string
+    numero: string
+}
