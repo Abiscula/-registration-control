de9868eb852b79db0c38f625cc82e9214e118524
Victor <victorabiscula@live.com>
2022-06-16 18:34:21 -0300

    criado nova rota para registro de contratos

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Menu } from './components/Menu';
 import DefaultPage from './pages/DefaultPage';
+import RegisterContract from './pages/RegisterContract';
 import RegisterPerson from './pages/RegisterPerson';
 
 export default function AppRouter() {
@@ -13,6 +14,8 @@ export default function AppRouter() {
           <Route path='/' element={<DefaultPage />} />
 
           <Route path='/register-person' element={<RegisterPerson />} />
+
+          <Route path='/register-contract' element={<RegisterContract />} />
 
         </Routes>
       </Router>
