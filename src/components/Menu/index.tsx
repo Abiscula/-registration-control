8ee2f1d235e1f058540d2dc369e08a5e6212761d
Victor <victorabiscula@live.com>
2022-06-17 11:59:25 -0300

    adicionado nova rota ao menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -9,7 +9,7 @@ export function Menu() {
                 <Link to="/" className="link"><House size={32} />Home</Link>
                 <Link to="/register-person" className="link"><PencilSimple size={32} />Cadastrar Pessoa</Link>
                 <Link to="/register-contract" className="link"><NotePencil size={32} />Cadastrar Contrato</Link>
-                <Link to="#" className="link"><Notepad size={32} />Contratos</Link>
+                <Link to="/contracts" className="link"><Notepad size={32} />Contratos</Link>
                 <Link to="#" className="link"><ChartLineUp size={32} />Estatísticas</Link>
             </nav>
         </Container>
