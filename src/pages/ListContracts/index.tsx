acc705e9a09f56162171754d4d86e95a993b78cd
Victor <victorabiscula@live.com>
2022-06-17 15:29:06 -0300

    criado filtro para contratos com base na validade

--- src/pages/ListContracts/index.tsx
+++ src/pages/ListContracts/index.tsx
@@ -8,16 +8,46 @@ import Contract from "../../components/Contract";
 import { contractProps } from "../../types";
 import { Container, Modal } from "./style";
 
+
 export default function ListContracts() {
 
     const [contracts, setContracts] = useState<contractProps[]>()
+    const [validContracts, setValidContracts] = useState<contractProps[]>()
     const [openModal, setOpenModal] = useState<boolean>(false)
     const [selectedContract, setSelectedContract] = useState<contractProps>()
 
-    const componentRef: any = useRef(null);
+    const componentRef = useRef(null);
     const handlePrint = useReactToPrint({
         content: () => componentRef?.current,
     });
+
+
+    function checkValidate(filterOption: string) {
+
+        if(filterOption === 'Todos') {
+            setValidContracts(contracts)
+        } else {
+            const option: any = {
+                "30 dias": 30,
+                "15 dias": 15,
+                "7 dias": 7,
+                "Hoje": 0
+            }
+            const optionDate = option[filterOption]
+            let filteredContracts: any = []
+            contracts?.forEach((contract: contractProps) => {
+                let partesData: any = contract.contractValidate.split("/");
+                const date = new Date(partesData[2], partesData[1] - 1, partesData[0]);
+                const today = new Date()
+                let newDate = today.setDate(today.getDate() + optionDate)
+                if(date <= new Date(newDate)) {
+                    filteredContracts.push(contract)
+                }
+            })
+            setValidContracts(filteredContracts)
+        }
+        }
+
 
     async function openContract(contractNumber: string) {
 
@@ -31,6 +61,7 @@ export default function ListContracts() {
         try {
             const api = await getAllContracts()
             setContracts(api)
+            setValidContracts(api)
         } catch (err) {
             console.log(err)
         }
@@ -38,14 +69,22 @@ export default function ListContracts() {
 
     useEffect(() => {
         listAllContracts()
+
     }, [])
 
     return (
         <Container>
             <h1 className="colorTitle">Lista de contatos</h1>
+            <select onChange={(e) => checkValidate(e.target.value)}>
+                <option>Todos</option>
+                <option>30 dias</option>
+                <option>15 dias</option>
+                <option>7 dias</option>
+                <option>Hoje</option>
+            </select>
             <section>
                 <div>
-                    {contracts?.map(({ contractNumber, nome, sobrenome, contractValidate }: contractProps) => (
+                    {validContracts?.map(({ contractNumber, nome, sobrenome, contractValidate }: contractProps) => (
 
                         <span key={contractNumber}>
                             <AddressBook
@@ -67,7 +106,7 @@ export default function ListContracts() {
                 <Modal>
                     <div>
                         <XSquare size={30} weight="light" className="btn-close" onClick={() => setOpenModal(false)} />
-                        <Contract data={selectedContract} componentRef={componentRef}/>
+                        <Contract data={selectedContract} componentRef={componentRef} />
                     </div>
                     <button className="btn-print" onClick={handlePrint}>Baixar contrato</button>
                 </Modal>
