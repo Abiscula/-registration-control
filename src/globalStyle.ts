b70d3a4e574f3e349e921006bbee1be7865a632d
Victor <victorabiscula@live.com>
2022-06-16 22:37:53 -0300

    adicionado estilo de botao e title ao global

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -10,6 +10,54 @@ export const Global = createGlobalStyle`
         min-width: 800px;
     }
     
+
+    .btn-div {
+        margin: 0 auto;
+        display: flex;
+        width: 110px;
+        height: 110px;
+        font-size: 1.1rem;
+        background-color: var(--background-menu);
+        border-radius: 150px;
+        align-items: center;
+        justify-content: center;
+        box-shadow: inset 0 -7px 0 rgba(0, 0, 0, 0.3), 0 5px 0 rgba(0, 0, 0, 0.15);
+
+    }
+
+
+    .btn-register {
+        border: none;
+        width: 90px;
+        height: 90px;
+        display: flex;
+        background: #ededed;
+        color: var(--color-gray);
+        font-weight: bold;
+        border-radius: 80px;
+        align-items: center;
+        justify-content: center;
+        box-shadow: inset 0 5px 0 rgba(0, 0, 0, 0.15);
+        cursor: pointer;
+  
+        &:hover {
+          transform: scale(1.1);
+        }
+
+    }
+
+    .colorTitle {
+        background-size: 100%;
+        background-color: var(--color--gray);
+        background-image: linear-gradient(to right, #8360c3, #2ebf91);
+        background-repeat: repeat;
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent; 
+        -moz-background-clip: text;
+        -moz-text-fill-color: transparent;
+    }
+
     
     #root > main.container {
         display: flex;
