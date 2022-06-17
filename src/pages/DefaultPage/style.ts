8edc6843575937002cac94e13a482b3f95a6cb1f
Victor <victorabiscula@live.com>
2022-06-16 22:39:06 -0300

    alteração no estilo

--- src/pages/DefaultPage/style.ts
+++ src/pages/DefaultPage/style.ts
@@ -6,24 +6,14 @@ export const Container = styled.main`
     justify-content: space-around;
     width: 800px;
     min-width: 600px;
-    height: 75vh;
+    height: 80vh;
     background-color: var(--color-white);
     border-radius: 60px;
     margin: auto;
     padding: 5rem;
 
-`
+    h1 {
+        font-size: 2.5rem;
+    }
 
-export const ColorTitle = styled.h1`
-    background-color: var(--color--gray);
-    background-image: linear-gradient(to right, #8360c3, #2ebf91);
-    font-size: 2.5rem;
-
-    background-size: 100%;
-    background-repeat: repeat;
-    -webkit-background-clip: text;
-    background-clip: text;
-    -webkit-text-fill-color: transparent; 
-    -moz-background-clip: text;
-    -moz-text-fill-color: transparent;
 `
