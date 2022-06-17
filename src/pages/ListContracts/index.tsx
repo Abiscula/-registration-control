cf018a0d0075d0332b1f06ad241be364f953ded7
Victor <victorabiscula@live.com>
2022-06-17 11:58:06 -0300

    criado página que lista os contratos

---
+++ src/pages/ListContracts/index.tsx
@@ -0,0 +1,71 @@
+import { AddressBook, XSquare } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { getAllContracts } from "../../api/backend";
+import Contract from "../../components/Contract";
+import { contractProps } from "../../types";
+import { Container, Modal } from "./style";
+
+export default function ListContracts() {
+
+    const [contracts, setContracts] = useState<contractProps[]>()
+    const [openModal, setOpenModal] = useState<boolean>(false)
+    const [selectedContract, setSelectedContract] = useState<contractProps>()
+
+
+    async function openContract(contractNumber: string) {
+
+        const contractData = contracts?.filter((contract) => contract.contractNumber === contractNumber)
+        setSelectedContract(contractData&&contractData[0])
+        console.log(contractData&&contractData[0])
+        setOpenModal(true)
+    }
+
+
+    async function listAllContracts() {
+        try {
+            const api = await getAllContracts()
+            setContracts(api)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        listAllContracts()
+    }, [])
+
+    return (
+        <Container>
+            <h1 className="colorTitle">Lista de contatos</h1>
+            <section>
+                <div>
+                    {contracts?.map(({ contractNumber, nome, sobrenome, contractValidate }: contractProps) => (
+
+                        <span key={contractNumber}>
+                            <AddressBook 
+                                size={60} 
+                                weight="light" 
+                                className="contract-icon"
+                                onClick={() => openContract(contractNumber)}
+                            />
+                            <label>{nome} {sobrenome}</label>
+                            <label>{contractValidate}</label>
+                        </span>
+
+                    ))}
+                </div>
+            </section>
+
+            {openModal 
+            ?
+            <Modal>
+                <div>
+                    <XSquare size={30} weight="light" className="btn-close" onClick={() => setOpenModal(false)}/>
+                    <Contract data={selectedContract}/>
+                </div>
+            </Modal>
+            :
+            ''}
+        </Container>
+    )
+}
