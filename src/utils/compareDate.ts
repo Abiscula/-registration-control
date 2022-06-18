7cc8d6b76990bd08a67b4557f6b2e6056acfdd06
Victor <victorabiscula@live.com>
2022-06-18 11:03:54 -0300

    adicionado validação para contratos vencidos

--- src/utils/compareDate.ts
+++ src/utils/compareDate.ts
@@ -5,10 +5,16 @@ export function compareDate(contracts: contractProps[], additionValue: number) {
     contracts?.forEach((contract: contractProps) => {
         let partesData: string[] | any = contract.contractValidate.split("/");
         const date: Date = new Date(partesData[2], partesData[1] - 1, partesData[0]);
-        const today = new Date()
-        let newDate = today.setDate(today.getDate() + additionValue)
-        if(date <= new Date(newDate)) {
-            filteredContracts.push(contract)
+        if (additionValue === -1) {
+            if (date < new Date()) {
+                filteredContracts.push(contract)
+            }
+        } else {
+            const today = new Date()
+            let newDate = today.setDate(today.getDate() + additionValue)
+            if ((date <= new Date(newDate)) && (date > new Date())) {
+                filteredContracts.push(contract)
+            }
         }
     })
     return filteredContracts
