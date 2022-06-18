425485cf0a3f7c9557f255b94ac6806a5ca82e92
Victor <victorabiscula@live.com>
2022-06-18 12:50:36 -0300

    adicionado possibilidade de escolher duração do contrato na hora do cadastro do mesmo

--- src/pages/RegisterContract/index.tsx
+++ src/pages/RegisterContract/index.tsx
@@ -14,7 +14,7 @@ interface optionProps {
 }
 
 export default function RegisterContract() {
-    
+
     const [person, setPerson] = useState<contractProps>({
         nome: '',
         sobrenome: '',
@@ -33,49 +33,53 @@ export default function RegisterContract() {
         contractValidate: ''
     })
     const [option, setOption] = useState<optionProps[]>()
+    const [personData, setPersonData] = useState<string>()
+    const [duration, setDuration] = useState<string>()
     const navigate = useNavigate()
 
     const date = new Date()
     const contractNumber = date.getTime()
     const contractDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-    const contractValidate = `${date.getDate()}/${date.getMonth() + 4}/${date.getFullYear()}`
 
 
     async function newContract() {
         try {
             const res = await createNewContract(person)
-            if(res === 200) {
+            if (res === 200) {
                 navigate('/')
                 alert(`Contrato nº ${person.contractNumber} criado em nome de ${person.nome}`)
             }
-        }catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
 
 
-    async function specifyPerson(personData: string) {
-        const cpf = personData.split(' ')[1]
+    async function specifyPerson() {
+        let contractValidate = `${date.getDate()}/${date.getMonth() + Number(duration) + 1}/${date.getFullYear()}`
 
         try {
-            const res = await getPerson(cpf)
-            setPerson({
-                nome: res[0].nome,
-                sobrenome: res[0].sobrenome,
-                cpf: res[0].cpf,
-                nasc: res[0].nasc,
-                email: res[0].email,
-                tel: res[0].tel,
-                cep: res[0].cep,
-                logradouro: res[0].logradouro,
-                localidade: res[0].localidade,
-                numero: res[0].numero,
-                uf: res[0].uf,
-                bairro: res[0].bairro,
-                contractNumber: String(contractNumber),
-                contractDate: String(contractDate),
-                contractValidate: String(contractValidate)
-            })
+            if (personData) {
+                const res = await getPerson(personData)
+                setPerson({
+                    nome: res[0].nome,
+                    sobrenome: res[0].sobrenome,
+                    cpf: res[0].cpf,
+                    nasc: res[0].nasc,
+                    email: res[0].email,
+                    tel: res[0].tel,
+                    cep: res[0].cep,
+                    logradouro: res[0].logradouro,
+                    localidade: res[0].localidade,
+                    numero: res[0].numero,
+                    uf: res[0].uf,
+                    bairro: res[0].bairro,
+                    contractNumber: String(contractNumber),
+                    contractDate: String(contractDate),
+                    contractValidate: contractValidate
+                })
+            }
+
         } catch (err) {
             console.log(err)
         }
@@ -85,7 +89,7 @@ export default function RegisterContract() {
         try {
             const people = await getAllPeople()
             const contracts = await getAllContracts()
-            
+
             let notExist = [].concat(
                 people.filter((p: any) => contracts.every((c: any) => p.cpf !== c.cpf)),
                 contracts.filter((c: any) => people.every((p: any) => c.cpf !== p.cpf))
@@ -99,31 +103,44 @@ export default function RegisterContract() {
     useEffect(() => {
         listPeople()
     }, [])
-    
+
 
     return (
         <Container>
-            <span>
-                <label className="colorTitle">Selecione um profissional</label>
-                <select onChange={(e) => specifyPerson(e.target.value)}>
-                    <option></option>
-                    {
-                        option?.map(({ cpf, nome, sobrenome }: optionProps) => (
-                            <option key={cpf}>
-                                {`CPF: ${cpf} - ${nome} ${sobrenome}`}
-                            </option>
-                        ))
-                    }
-                </select>
-            </span>
+            <div>
+                <span>
+                    <label className="colorTitle">Selecione um profissional</label>
+                    <select onChange={(e) => setPersonData(e.target.value.split(' ')[1])}>
+                        <option></option>
+                        {
+                            option?.map(({ cpf, nome, sobrenome }: optionProps) => (
+                                <option key={cpf}>
+                                    {`CPF: ${cpf} - ${nome} ${sobrenome}`}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </span>
+                <aside>
+                    <label className="colorTitle">Validade do contrato</label>
+                    <span>
+                        <input
+                            type="number"
+                            className="input-validate"
+                            onChange={(e) => setDuration(e.target.value)}
+                        />
+                        <button disabled={!duration} onClick={() => specifyPerson()}>Abrir</button>
+                    </span>
+                </aside>
+            </div>
 
-            {person.cpf 
-            ?   <Contract data={person} /> 
-            :   <section>
+            {person.cpf && duration
+                ? <Contract data={person} />
+                : <section>
                     <ArrowBendUpRight size={150} className="icon" weight="light" />
-                    <FileSearch size={200} className="icon" weight="thin"/>
+                    <FileSearch size={200} className="icon" weight="thin" />
                     <ArrowBendDownLeft size={150} className="icon" weight="light" />
-                
+
                 </section>}
 
 
