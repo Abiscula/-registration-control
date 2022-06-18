2fe0382fe1e929b5b08920b541bf8e091683a39e
Victor <victorabiscula@live.com>
2022-06-18 16:15:18 -0300

    adicionado componente da modal

--- src/pages/DefaultPage/index.tsx
+++ src/pages/DefaultPage/index.tsx
@@ -1,9 +1,11 @@
+import NotifyModal from "../../components/NotifyModal";
 import { Container } from "./style";
 
 export default function DefaultPage() {
     return (
         <Container>
             <div>
+                <NotifyModal />
                 <h1 className="colorTitle">Painel de administração</h1>
             </div>
         </Container>
